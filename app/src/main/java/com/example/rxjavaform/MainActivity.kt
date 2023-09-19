c0efb454ca73a19883f102916fd2aad043886c1e
Jodi Kurniawan <jodikr8s@gmail.com>
2023-09-19 21:58:34 +0700

    Add rxjava for form and button; Add merge and combinelatest

--- app/src/main/java/com/example/rxjavaform/MainActivity.kt
+++ app/src/main/java/com/example/rxjavaform/MainActivity.kt
@@ -2,10 +2,84 @@ package com.example.rxjavaform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import androidx.core.content.ContextCompat
+import com.example.rxjavaform.databinding.ActivityMainBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val emailStream = RxTextView.textChanges(binding.edEmail)
+            .skipInitialValue()
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        emailStream.subscribe {
+            showEmailExistAlert(it)
+        }
+
+        val passwordStream = RxTextView.textChanges(binding.edPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.length < 6
+            }
+        passwordStream.subscribe {
+            showPasswordMinimalAlert(it)
+        }
+
+        val passwordConfirmationStream = Observable.merge(
+            RxTextView.textChanges(binding.edPassword)
+                .map { password ->
+                    password.toString() != binding.edConfirmPassword.text.toString()
+                },
+            RxTextView.textChanges(binding.edConfirmPassword)
+                .map { confirmPassword ->
+                    confirmPassword.toString() != binding.edPassword.text.toString()
+                }
+        )
+        passwordConfirmationStream.subscribe {
+            showPasswordConfirmationAlert(it)
+        }
+
+        val invalidFieldsStream = Observable.combineLatest(
+            emailStream,
+            passwordStream,
+            passwordConfirmationStream,
+            object : Function3<Boolean, Boolean, Boolean, Boolean> {
+                override fun invoke(emailInvalid: Boolean, passwordInvalid: Boolean, passwordConfirmationInvalid: Boolean): Boolean {
+                    return !emailInvalid && !passwordInvalid && !passwordConfirmationInvalid
+                }
+            })
+        invalidFieldsStream.subscribe { isValid ->
+            if (isValid) {
+                binding.btnRegister.isEnabled = true
+                binding.btnRegister.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+            } else {
+                binding.btnRegister.isEnabled = false
+                binding.btnRegister.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+            }
+        }
+
+    }
+
+    private fun showEmailExistAlert(isNotValid: Boolean) {
+        binding.edEmail.error = if (isNotValid) getString(R.string.email_not_valid) else null
+    }
+
+    private fun showPasswordMinimalAlert(isNotValid: Boolean) {
+        binding.edPassword.error = if (isNotValid) getString(R.string.password_not_valid) else null
+    }
+
+    private fun showPasswordConfirmationAlert(isNotValid: Boolean) {
+        binding.edConfirmPassword.error = if (isNotValid) getString(R.string.password_not_same) else null
     }
 }
